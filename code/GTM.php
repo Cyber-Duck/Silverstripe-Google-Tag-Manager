d558421933bb6ee6bddda8b4e4c8f26c2616b020
Andrew McCormack <andrewm@cyber-duck.co.uk>
2015-08-28 13:37:02 +0100

    code fix

--- code/GTM.php
+++ code/GTM.php
@@ -20,7 +20,7 @@ class GTM {
 		'<!-- Google Tag Manager -->
 		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TJFKGH"
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
 		new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
 		j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
 		\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
@@ -136,7 +136,6 @@ class GTM {
 			endforeach;
 			self::$val .= ']';
 
-				
 			self::$val .= "}";
 			self::$val .= "}";
 		endif;
